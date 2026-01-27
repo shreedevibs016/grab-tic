d773e8ecab3d4f8b3d13a7feca54cc1d844da0c4
Shreedevi <shreedevibs016@gmail.com>
2026-01-27 10:48:15 +0530

    hosting setup

--- grab_tic/grab_tic/settings.py
+++ grab_tic/grab_tic/settings.py
@@ -84,23 +84,26 @@ WSGI_APPLICATION = 'grab_tic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER':config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST':config('DB_HOST'),
-        'PORT':'3306',
-    }
-}
+if config('ENVIRONMENT') == 'production' :
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+                    }
+                }
+else :
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': config('DB_NAME'),
+            'USER':config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST':config('DB_HOST'),
+            'PORT':'3306',
+                    }
+                }
 
 
 # Password validation
